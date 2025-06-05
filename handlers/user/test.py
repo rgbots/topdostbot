2e8b02610236e3ed2979b661569abc5e7cbb51d0
Nelox69 <neloxdb@gmail.com>
2025-06-05 14:55:20 +0500

    test fixed

--- handlers/user/test.py
+++ handlers/user/test.py
@@ -1,69 +1,69 @@
 class OneQuestion:
     text = "🏝 Siz chet elda bo'lganmisiz?"
     text1 = "🏝 U chet elda bo'lganmisiz?"
-    photo = "AgACAgQAAxkBAAIBjWXMrR0UOi5VUwYHqT35M4QLbvnEAAK7zTEbDNPgUJ-l0U2bP0JmAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/54e0042281a67dc42c7f9.jpg"
     ans = ["🕶 Ha", "😖 Yo'q"]
     row_width = 1
 
 class TwoQuestion:
     text = "🎂 Sizning tug'ilgan kuniz qachon?"
     text1 = "🎂 Uning tug'ilgan kuniz qachon?"
-    photo = "AgACAgQAAxkBAAIBjmXMrR6m9bEmD7uMfwkzAy77v9rcAAK8zTEbDNPgUEXhGEMOrpjDAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/516930f2ef687775fcb08.jpg"
     ans = ["🍁 Kuzda", "☃️ Qishda", "🌸 Bahorda", "🌤 Yozda"]
     row_width = 2
 
 class ThreeQuestion:
     text = "🎨 Oz'ingizni natural sochingizni rangi qanday?"
     text1 = "🎨 Uning natural sochining rangi qanday?"
-    photo = "AgACAgQAAxkBAAIBkGXMrSD5uA-f4y7_HlhRPT0Mlm_3AAK9zTEbDNPgULFaNNsVIRp4AQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/3cbf7f0fb5e2f03f967ee.jpg"
     ans = ["👱‍♀️ Blond", "🌾 Sariq", "🌰 Jigarrang", "🦊 O'lo'v", "🖤 Qora", "👀 Boshqa"]
     row_width = 2
 
 class FourQuestion:
     text = "🏫 Siz maktabda qanday o'qiysiz?"
     text1 = "🏫 U maktabda qanday o'qiydi?"
-    photo = "AgACAgQAAxkBAAIBkmXMrSLDkKDAlyv1kXRXpTFi62_JAAK-zTEbDNPgULmOUXufGSk6AQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/a512761e9a194623dc676.jpg"
     ans = ["🤓 Aloga", "🎒 Yaxshi", "🙄 Uchga", "🥱 Qo'yganim bor", "🗿 Omadim kelsa"]
     row_width = 1
 
 class FiveQuestion:
     text = "❓ Dadangizni ismidagi birinchi xarifi nima?"
     text1 = "❓ Uning dadasini ismidagi birinchi xarifi nima?"
-    photo = "AgACAgQAAxkBAAIBlGXMrSfjv6jP23D1GhzY8kgCv-R_AALAzTEbDNPgUCDZW4wOiQABAAMCAAN5AAMwBA"
+    photo = "https://telegra.ph/file/2c3393db39b1f375745e2.jpg"
     ans = ["✏️ A-B-D", "✏️ E-F-G", "✏️ H-I-J", "✏️ К-L-M", "✏️ N-O-P", "✏️ Q-R-S", "✏️ T-U-V", "✏️ W-X-Y", "✏️ Z-O'-G'", "✏️ SH-CH"]
     row_width = 3
 
 class SixQuestion:
     text = "💘 Siz munosabatda bo'lganmisiz?"
     text1 = "💘 U munosabatda bo'lganmi?"
-    photo = "AgACAgQAAxkBAAIBlmXMrSm7u3Q8_WUgZ7Apkq3nMniQAALEzTEbDNPgUGj59Kj5LkV1AQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/18adeaa73ad70e015aa04.jpg"
     ans = ["✔️ Ha", "😐 Hechqachon"]
     row_width = 1
 
 class SevenQuestion:
     text = "🍴Siz qayerda ovqatlanishni yoqtirasiz?"
     text1 = "🍴U qayerda ovqatlanishni yoqtiradi?"
-    photo = "AgACAgQAAxkBAAIBmGXMrSoxZ5EmUM3_xzUvRt9jxsw3AALFzTEbDNPgUMzGcE0UtlsQAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/91d5ad0f33f2a0dc49ac7.jpg"
     ans = ["🍗 KFC", "🍟 Burger King", "🍔 Mak donalds", "🍞 Nonvoy xona", "🏡 Uyda", "🍱 Oshxona", "👫 Do'stlarimikida", "🍽 Restoranda"]
     row_width = 2
 
 class EightQuestion:
     text = "📞 Telefoningiz qanday?"
     text1 = "📞 Uning telefoni qanday?"
-    photo = "AgACAgQAAxkBAAIBmWXMrcFzEzR59K-E3cpMQKyjKAP4AALJzTEbDNPgUJLh3LVP8gfRAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/6e492d89e637199584ef9.jpg"
     ans = ["🤖 Android", "🍏 Iphone"]
     row_width = 1
 
 class NineQuestion:
     text = "👟 Oyoq kiymingizni hajmi necha?"
     text1 = "👟 Uning oyoq kiymingizni hajmi necha?"
-    photo = "AgACAgQAAxkBAAIBmmXMrccW6D0Be6gpxYwM-fkaVi9rAALKzTEbDNPgUKp5eR7aXfXyAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/15656e0d8e3b0f83d5e26.jpg"
     ans = ["💫 25 - 30", "🎀 31 - 34", "🩴 35 - 38", "👞 39 - 42", "🥾 42+"]
     row_width = 2
 
 class TenQuestion:
     text = "👇 Shuning ichidan nima qilishni hohlaysiz?"
     text1 = "👇 U shuning ichidan nima qilishni hohlaydi?"
-    photo = "AgACAgQAAxkBAAIBm2XMrctAeKRU7XnqpTR4xFdTmExiAALMzTEbDNPgUAgJOVacPevVAQADAgADeQADMAQ"
+    photo = "https://telegra.ph/file/36c7033f60b2d5d506a6b.jpg"
     ans = ["🤫 Pirsing", "🕷 Tatu", "🪄 Maktabni almashtirish", "🎉 Vecherinkaga borish", "🎨 Sochni bo'yash", "🙄 Hechnarsa"]
     row_width = 2
